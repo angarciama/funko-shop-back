137f8b4e9a492138508f372268ef1af985d686d7
angarciama <angarciama@unal.edu.co>
2024-03-29 12:13:50 -0500

    [Advance] -app -config db -errorHandler

--- app.js
+++ app.js
@@ -3,9 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const errorHandler = require('./utils/errorHandler');
 const sequelize = require('./config/database'); // Importar la instancia de Sequelize
-const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
-const productRoutes = require('./routes/productRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,28 +12,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Rutas de la aplicación
-app.use('/api/auth', authRoutes);
-app.use('/api/users', userRotes);
-app.use('/api/products', productRoutes);
-
-//Rutas
-const mainRoutes = require('./routes/mainRoutes');
-const userRoutes = require('./routes/userRoutes');
-const productsRoutes = require('./routes/productsRoutes');
-const productsApiRoutes = require('./routes/api/productsApiRoutes');
-const publicPath = path.resolve(__dirname, "./public");
-//Rutas API
-app.use('/api/products', require('./routes/api/productsApiRoutes'));
-//Template Engine
-app.set('view engine', 'ejs');
-app.use(express.json());//permite procesar los datos de JSON para API
-app.use(express.urlencoded({ extended: false }));//permite procesar formularios
-app.use(express.static(publicPath));
-app.use(mainRoutes);
-// Routes
-app.use('/user', userRoutes);
-app.use('/products', productsRoutes);
-//Api Rotes
+app.use('/api/users', userRoutes);
 
 // Manejador de errores
 app.use(errorHandler);
