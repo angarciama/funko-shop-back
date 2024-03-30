dc36e152bf7c06b54975bca3925362cc713f251c
angarciama <angarciama@unal.edu.co>
2024-03-29 21:59:51 -0500

    [Advance] -verifyToken: ajustes

--- controllers/authController.js
+++ controllers/authController.js
@@ -51,11 +51,16 @@ exports.verifyToken = (req, res, next) => {
         return res.status(401).json({ msg: 'No hay token, permiso no válido' });
     }
 
+    // Extraer el token de la cadena "Bearer token"
+    const tokenParts = token.split(' ');
+    const tokenString = tokenParts[1]; // La parte después de "Bearer"
+
     // Verificar el token
     try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const decoded = jwt.verify(tokenString, process.env.JWT_SECRET);
         req.user = decoded.user;
-        next();
+
+        res.status(200).json({ msg: 'Token válido' });
     } catch (error) {
         res.status(401).json({ msg: 'Token no válido' });
     }
